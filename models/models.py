342fc955870e21edbedb47664732a5b72c7e8e1f
MajidEi <abdelmajededdahbi@gmail.com>
2021-05-12 17:09:41 +0000

    some changes in import_file method

--- models/models.py
+++ models/models.py
@@ -10,75 +10,101 @@ import csv
 import io
 import xlrd
 
+
 from odoo import models, fields, api, _
 from odoo.exceptions import Warning, UserError
 
 class import_records(models.Model):
     _name = 'import_records.import_records'
     _inherit = 'base'
-    file = fields.Binary(string='CSV File', required=True)
-    file_type = fields.Char()
-    model = 'invest.invest'
-    fieldsList = []
-    data = []
-    options = {'headers': True, 'advanced': True, 'keep_matches': False, 'encoding': 'utf-8', 'separator': ',', 'quoting': '"', 'date_format': '', 'datetime_format': '', 'float_thousand_separator': ',', 'float_decimal_separator': '.', 'fields': []}
+    
+    file = fields.Binary('File to import', required=True)
+    file_type = fields.Selection([('csv', 'CSV'), ('xls', 'XLS')], string="File Type", default='csv')
 
-    @api.model
-    def get_fields(self, model, depth=2):
-        Model = self.env[model]
-        importable_fields = [{
-            'id': 'id',
-            'name': 'id',
-            'string': _("External ID"),
-            'required': False,
-            'fields': [],
-            'type': 'id',
-        }]
-        model_fields = Model.fields_get()
-        blacklist = models.MAGIC_COLUMNS + [Model.CONCURRENCY_CHECK_FIELD]
-        for name, field in model_fields.items():
-            if name in blacklist:
-                continue
-            # an empty string means the field is deprecated, @deprecated must
-            # be absent or False to mean not-deprecated
-            if field.get('deprecated', False) is not False:
-                continue
-            if field.get('readonly'):
-                states = field.get('states')
-                if not states:
+    res_model = fields.Selection([('explo.explo','Explo'), ('invst.invest','Invest')],string='Model')
+
+    def import_file(self):
+        table = 'NULL'
+        if self.res_model == 'explo.explo':
+            table = 'explo_explo'
+        if self.res_model == 'invest.invest':
+            table = 'invest_invest'
+        if self.file_type == 'csv':
+
+            keys = ['code', 'rubriques','montant']
+
+            try:
+                file = base64.b64decode(self.file)
+                data = io.StringIO(file.decode("utf-8"))
+                data.seek(0)
+                file_reader = []
+                csv_reader = csv.reader(data, delimiter=',')
+                file_reader.extend(csv_reader)
+
+            except:
+                raise Warning(_("File is not Valid!"))
+
+            fields = list(map(str, file_reader[0]))
+            print(fields)
+            for fr in range(len(file_reader)):
+                line = list(map(str, file_reader[fr]))
+                vals = dict(zip(keys, line))
+                if vals:
+                    if fr == 0:
+                        continue
+                    else:
+                        vals.update({
+                            'code': line[0],
+                            'rubriques': line[1],
+                            'montant': line[2],
+                        })
+                        if line[2] == '' : 
+                            line[2] = '0.0'
+                        if "'" in line[1] :    
+                            line[1] = line[1].replace("'","\'")
+                        self.env.cr.execute("insert into "+table+" (code,rubriques,montant) values('"+line[0]+"','"+line[1]+"','"+line[2]+"')")
+                        self.env.cr.commit()
+                        
+            
+                        
+        elif self.file_type == 'xls':
+            try:
+                fp = tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx")
+                fp.write(binascii.a2b_base64(self.file))
+                fp.seek(0)
+                vals = {}
+                workbook = xlrd.open_workbook(fp.name)
+                sheet = workbook.sheet_by_index(0)
+
+            except:
+                raise Warning(_("File not Valid"))
+            
+            fields = map(lambda row: row.value.encode('utf-8'), sheet.row(0))
+            print(fields)
+            for row_no in range(sheet.nrows):
+                val = {}
+                if row_no <= 0:
                     continue
-                # states = {state: [(attr, value), (attr2, value2)], state2:...}
-                if not any(attr == 'readonly' and value is False
-                           for attr, value in itertools.chain.from_iterable(states.values())):
-                    continue
-            field_value = {
-                'id': name,
-                'name': name,
-                'string': field['string'],
-                # Y U NO ALWAYS HAS REQUIRED
-                'required': bool(field.get('required')),
-                'fields': [],
-                'type': field['type'],
-            }
+                else:
 
-            if field['type'] in ('many2many', 'many2one'):
-                field_value['fields'] = [
-                    dict(field_value, name='id', string=_("External ID"), type='id'),
-                    dict(field_value, name='.id', string=_("Database ID"), type='id'),
-                ]
-            elif field['type'] == 'one2many' and depth:
-                field_value['fields'] = self.get_fields(field['relation'], depth=depth-1)
-                if self.user_has_groups('base.group_no_one'):
-                    field_value['fields'].append({'id': '.id', 'name': '.id', 'string': _("Database ID"), 'required': False, 'fields': [], 'type': 'id'})
+                    line = list(
+                        map(lambda row: isinstance(row.value, bytes) and row.value.encode('utf-8') or str(row.value),
+                            sheet.row(row_no)))
 
-            importable_fields.append(field_value)
-
-        # TODO: cache on model?
-        return importable_fields
+                    vals.update({
+                        'code': line[0],
+                        'rubriques': line[1],
+                        'montant': line[2],
+                    })
+                    if line[2] == '' :
+                        line[2] = '0.0'
+                    if "'" in line[1] :
+                        line[1] = line[1].replace("'","\'")
+                    self.env.cr.execute("insert into "+table+" (code,rubriques,montant) values('"+line[0]+"','"+line[1]+"','"+line[2]+"')")
+                    self.env.cr.commit()     
+        else:
+            raise UserError(_("Please select xls or csv format!"))
 
     
     
-
-
-    
-    
+    #tim lif 
